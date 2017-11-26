114531bbbb2036caf48a9c10f98d991b783ddce6
Vahid Vaezian <vahid.vaezian@gmail.com>
2017-11-25 23:54:36 -0800

    Added Edit page (on Progress)

---
+++ app/src/main/java/com/example/vvaezian/multilingovocabularypractice/DatabaseHelper.java
@@ -0,0 +1,60 @@
+package com.example.vvaezian.multilingovocabularypractice;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by vvaezian on 25/11/17.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "vocab.db";
+    public static final String TABLE_NAME = "myTable";
+    public static final String COL_1 = "id";
+    public static final String COL_2 = "en";
+    public static final String COL_3 = "fr";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " ( id INTEGER PRIMARY KEY AUTOINCREMENT, en TEXT, fr TEXT); ");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String en, String fr){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, en);
+        contentValues.put(COL_3, fr);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[] {id} );
+    }
+
+}
---
+++ app/src/main/java/com/example/vvaezian/multilingovocabularypractice/EditTablePage.java
@@ -0,0 +1,104 @@
+package com.example.vvaezian.multilingovocabularypractice;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class EditTablePage extends AppCompatActivity {
+
+    DatabaseHelper myDB;
+    EditText etEN, etFR, etDelete;
+    Button btnAddData;
+    Button btnShowData;
+    Button btnDelete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_table_page);
+
+        etEN = (EditText) findViewById(R.id.etEN);
+        etFR = (EditText) findViewById(R.id.etFR);
+        etDelete = (EditText)  findViewById(R.id.etDelete);
+        btnAddData = (Button) findViewById(R.id.btnAddData);
+        btnShowData = (Button) findViewById(R.id.btnShowData);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+
+        myDB = new DatabaseHelper(this);
+
+        AddData();
+        ShowData();
+        DeleteRow();
+    }
+
+    public void AddData(){
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean isInserted = myDB.insertData(etEN.getText().toString(),
+                                        etFR.getText().toString());
+                        if (isInserted)
+                            Toast.makeText(EditTablePage.this, "Data Inserted Successfully", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(EditTablePage.this, "Data Insertion Failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+
+    public void ShowData() {
+        btnShowData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Cursor res = myDB.getAllData();
+                        if (res.getCount() == 0) {
+                            showMessage("Error", "Nothing Found!");
+                            return;
+                        }
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("IDoracle : " + res.getString(0) + "\n");
+                            buffer.append("EN: " + res.getString(1) + "\n");
+                            buffer.append("FR: " + res.getString(2) + "\n\n");
+                        }
+
+                        showMessage("Data", buffer.toString());
+
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title, String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
+    public void DeleteRow() {
+        btnDelete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Integer deletedRows = myDB.deleteData(etDelete.getText().toString());
+                        if (deletedRows > 0)
+                            Toast.makeText(EditTablePage.this, "Data Deleted Successfully", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(EditTablePage.this, "Data Deletion Failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+
+}
