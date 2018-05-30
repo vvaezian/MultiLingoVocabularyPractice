4cee7e5b37ebb85b33b6038f2ce9b4439cbd0a18
Vahid Vaezian <vahid.vaezian@gmail.com>
2018-05-30 16:11:45 -0700

    added: clicking flags makes them transparent and the text is revealed

--- app/src/main/java/com/example/vvaezian/multilingovocabularypractice/testPage.java
+++ app/src/main/java/com/example/vvaezian/multilingovocabularypractice/testPage.java
@@ -16,38 +16,48 @@ public class testPage extends AppCompatActivity {
         setContentView(R.layout.test);
 
         TableLayout tl = (TableLayout) findViewById(R.id.LanguageFlagsArea);
-        String[] langs = new String[]{"French", "German", "Spanish"};  // This will be fetched from the duolingo-api
+        final String[] langs = new String[]{"French", "German", "Spanish"};  // This will be fetched from the duolingo-api
         TableRow[] rows = new TableRow[langs.length];
         //Button[] buttons = new Button[langs.length];
-        TextView[] buttons = new TextView[langs.length];
+        final TextView[] buttons = new TextView[langs.length];
 
         for (int i=0; i < langs.length; i++){
 
-            /* Create a new row to be added. */
+            final String langsElement = langs[i];  // the language of index i. Since it will be used "globally", needs to be declared final here
+
+            // Create a new row to be added.
             rows[i] = new TableRow(this);
             TableLayout.LayoutParams params = new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,TableLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(0, 80, 0, 80);
+            params.setMargins(0, 40, 0, 40);
             rows[i].setLayoutParams(params);
 
-            /* Create a textview to be the row-content. */
+            // Create a button to be the row-content.
             buttons[i] = new Button(this);
-            buttons[i].setText(langs[i]);
-            //buttons[i].setBackgroundColor(0xFF00FF00);
-            buttons[i].setBackgroundResource(R.drawable.france);
+            buttons[i].setText("");
 
+            // put flag of language at index 'i' in the the background of the button
+            String flagName = langsElement.toLowerCase();
+            int resID = getResources().getIdentifier(flagName , "drawable", getPackageName());
+            buttons[i].setBackgroundResource(resID);
+
+            // clicking the button makes the flag transparent and the text visible
             buttons[i].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Button button = (Button) v;
-                        button.setVisibility(View.INVISIBLE);
+
+                        String transparentFlagName = langsElement.toLowerCase() + "_transparent";
+                        int resID = getResources().getIdentifier(transparentFlagName , "drawable", getPackageName());
+                        button.setBackgroundResource(resID);
+                        button.setBackgroundResource(resID);
+                        button.setText(langsElement);
                     }
-                });
-            /* Add Button to row. */
+            });
+
+            // Add the button to row.
             rows[i].addView(buttons[i]);
 
-
-            /* Add row to TableLayout. */
-            //rows[i].setBackgroundResource(R.drawable.france);
+            // Add the row to TableLayout.
             tl.addView(rows[i], params);
         }
 
