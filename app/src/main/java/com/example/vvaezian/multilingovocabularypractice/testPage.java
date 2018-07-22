a5553e701864a84e5cf7efb15ad4dc5e9897dd9b
Vahid Vaezian <vahid.vaezian@gmail.com>
2018-07-21 20:23:31 -0700

    added delay and scroll to top

--- app/src/main/java/com/example/vvaezian/multilingovocabularypractice/testPage.java
+++ app/src/main/java/com/example/vvaezian/multilingovocabularypractice/testPage.java
@@ -3,11 +3,13 @@ package com.example.vvaezian.multilingovocabularypractice;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -101,9 +103,14 @@ public class testPage extends ActionBar {
         }
     }
 
-    //Button next = (Button) findViewById(R.id.BtnNext);
+
+    //
     public void BtnNextClicked(View view) {
 
+        ScrollView scroll = (ScrollView) findViewById(R.id.scrolView);
+        scroll.setFocusableInTouchMode(true);
+        //scroll.setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
+        scroll.fullScroll(View.FOCUS_UP);
 
         // getting user's languages from shared preferences
         final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
@@ -112,48 +119,60 @@ public class testPage extends ActionBar {
         final String[] langs = langsConcated.split(" ");
 
         // programmatically create fields to hold flags and translations
-        TableLayout tl = (TableLayout) findViewById(R.id.LanguageFlagsArea);
+        final TableLayout tl = (TableLayout) findViewById(R.id.LanguageFlagsArea);
 
         // clear the page
         tl.removeAllViews();
+        final Button btnNext = (Button) findViewById(R.id.BtnNext);
+        btnNext.setVisibility(View.GONE);
 
-        TableRow[] rows = new TableRow[langs.length];
-        final TextView[] buttons = new TextView[langs.length];
+        // Handler will make the inside code be executed with delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                TableRow[] rows = new TableRow[langs.length];
+                final TextView[] buttons = new TextView[langs.length];
 
-        for (int i=0; i < langs.length; i++){
+                for (int i=0; i < langs.length; i++){
 
-            final String langsElement = langs[i];  // the language at index i.
+                    final String langsElement = langs[i];  // the language at index i.
 
-            // Create a new row to be added.
-            rows[i] = new TableRow(this);
-            TableLayout.LayoutParams params = new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,TableLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(0, 40, 0, 40);
-            rows[i].setLayoutParams(params);
+                    // Create a new row to be added.
+                    rows[i] = new TableRow(getApplicationContext());
+                    TableLayout.LayoutParams params = new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT,TableLayout.LayoutParams.WRAP_CONTENT);
+                    params.setMargins(0, 40, 0, 40);
+                    rows[i].setLayoutParams(params);
 
-            // Create a button to be the row-content.
-            buttons[i] = new Button(this);
+                    // Create a button to be the row-content.
+                    buttons[i] = new Button(getApplicationContext());
 
-            // putting flag of the language at index 'i', in the the background of the button
-            int resID = getResources().getIdentifier(langsElement , "drawable", getPackageName()); // resID is id of the resource with the name in langsElement
-            buttons[i].setBackgroundResource(resID);
+                    // putting flag of the language at index 'i', in the the background of the button
+                    int resID = getResources().getIdentifier(langsElement , "drawable", getPackageName()); // resID is id of the resource with the name in langsElement
+                    buttons[i].setBackgroundResource(resID);
 
-            // clicking the button makes the flag transparent and the text visible
-            buttons[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Button button = (Button) v;
+                    // clicking the button makes the flag transparent and the text visible
+                    buttons[i].setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Button button = (Button) v;
 
-                    int resID = getResources().getIdentifier(langsElement + "_transparent" , "drawable", getPackageName());
-                    button.setBackgroundResource(resID);
-                    button.setText("test");
+                            int resID = getResources().getIdentifier(langsElement + "_transparent" , "drawable", getPackageName());
+                            button.setBackgroundResource(resID);
+                            button.setText("test");
+                        }
+                    });
+
+                    // Add the button to row.
+                    rows[i].addView(buttons[i]);
+
+                    // Add the row to TableLayout.
+                    tl.addView(rows[i], params);
                 }
-            });
 
-            // Add the button to row.
-            rows[i].addView(buttons[i]);
+                btnNext.setVisibility(View.VISIBLE);
+            }
+        }, 200);   //.2 seconds
 
-            // Add the row to TableLayout.
-            tl.addView(rows[i], params);
-        }
+
     }
 }
